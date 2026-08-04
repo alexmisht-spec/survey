5667daeb381ffa2821511bd09801b62716eff9f8
Nimrod-ogoro <nimrodogoro@gmail.com>
2026-08-04 15:31:52 +0300

    error fixede

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,64 +1,37 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://localhost:5000/api",
-
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
-
     headers: {
-
         "Content-Type": "application/json",
-
     },
-
 });
 
 api.interceptors.request.use((config) => {
-
     const token = localStorage.getItem("accessToken");
 
     if (token) {
-
         config.headers.Authorization = `Bearer ${token}`;
-
     }
 
     return config;
-
 });
 
-/*
-|--------------------------------------------------------------------------
-| GLOBAL RESPONSE INTERCEPTOR
-|--------------------------------------------------------------------------
-*/
-
 api.interceptors.response.use(
-
     (response) => response,
-
     (error) => {
-
         if (error.response?.status === 401) {
-
             localStorage.removeItem("accessToken");
 
-            // Prevent infinite redirect loop
             if (window.location.pathname !== "/login") {
-
                 alert("Your session has expired. Please login again.");
-
                 window.location.href = "/login";
-
             }
-
         }
 
         return Promise.reject(error);
-
     }
-
 );
 
 export default api;
