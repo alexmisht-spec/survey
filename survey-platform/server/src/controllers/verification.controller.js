b07f2c6c3c3ce43c253c5458a20dbf09ce9d2a6c
Nimrod-ogoro <nimrodogoro@gmail.com>
2026-08-06 11:09:59 +0300

    final fix

--- survey-platform/server/src/controllers/verification.controller.js
+++ survey-platform/server/src/controllers/verification.controller.js
@@ -1,7 +1,7 @@
 import prisma from "../config/prisma.js";
-import "../config/cloudinary.js";
-import  uploadToCloudinary  from "../utilis/uploadToCloudinary.js";
-import  createNotification  from "../utilis/notification.js";
+import cloudinary from "../config/cloudinary.js";
+import { uploadToCloudinary } from "../utilis/uploadToCloudinary.js";
+import { createNotification } from "../utilis/notification.js";
 
 function getPublicId(url) {
 
