1e985b37863753742af6145a6adb20b2d06aa4f9
Nimrod-ogoro <nimrodogoro@gmail.com>
2026-08-10 15:52:21 +0300

    users

--- client/src/api/admin.email.js
+++ client/src/api/admin.email.js
@@ -1,30 +1,41 @@
+
 import api from "./axios";
 
 /*
 |--------------------------------------------------------------------------
-| GET UNVERIFIED USERS
+| UNVERIFIED USERS
 |--------------------------------------------------------------------------
 */
 
 export const getUnverifiedUsers = () =>
-    api.get("/admin/email/unverified-users");
+    api.get("/admin/email/users/unverified");
 
 
 /*
 |--------------------------------------------------------------------------
-| GET EMAIL TEMPLATES
+| EMAIL TEMPLATES
 |--------------------------------------------------------------------------
 */
 
 export const getEmailTemplates = () =>
     api.get("/admin/email/templates");
 
+export const createEmailTemplate = (data) =>
+    api.post("/admin/email/templates", data);
+
+export const updateEmailTemplate = (id, data) =>
+    api.put(`/admin/email/templates/${id}`, data);
+
+export const deleteEmailTemplate = (id) =>
+    api.delete(`/admin/email/templates/${id}`);
+
 
 /*
 |--------------------------------------------------------------------------
-| SEND EMAIL TO USER
+| SEND EMAIL
 |--------------------------------------------------------------------------
 */
 
 export const sendAdminEmail = (data) =>
     api.post("/admin/email/send", data);
+
